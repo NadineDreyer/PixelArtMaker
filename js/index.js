15869ef7ff71a71776285835ef3f84eb5ab58e14
Nadine <nadine@payteq.com>
2020-05-25 20:58:30 +0200

    #02 | Added listener to click event to apply color

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
-// Select color input
-
 const pixelTable = document.getElementById('pixelCanvas');
 
 // Get user input
 function userSubmit() {
+    //Cancel event
     event.preventDefault();
 
     const specHeight = document.getElementById("inputHeight").value;
@@ -12,45 +11,45 @@ function userSubmit() {
     makeGrid(specWidth, specHeight)
 }
 
-//Populate grid
+//Populate grids
 function makeGrid(width, height) {
-    console.error(width + height);
-
+    //Clear table
     pixelTable.innerHTML = '';
 
+    //Create Rows and Columns
     createRowsColumns(height, width);
 
-    
-    //get canvas to apply rows and columns
-
-    //add grid into table element
-    // Add click event to grid cells once the table grid has been created
+    //Apply color to cell/s
+    clickEventToApplyColor();
 }
 
+//Create Rows and Columns
 function createRowsColumns(height, width) {
     for (let x = 0; x < height; x++) {
         let row = document.createElement("tr");
-        row.innerText = x + 1;
-        pixelTable.appendChild(row).className = "row";
 
         for (let y = 0; y < width; y++) {
-            console.warn("REACHED2");
             let addColumn = document.createElement("td");
-            console.warn(row[0]);
-            addColumn.innerText = y+1;
-            row.appendChild(addColumn).className = "column";
+            row.appendChild(addColumn).className = "cell";
         }
+        pixelTable.appendChild(row).className = "row";
     }
 }
 
-//Allow user to select grid item to apply color to
-function clickEventToCells() {
-    //on color selection return color:
+//Allow user to select grid item to apply color
+function clickEventToApplyColor() {
+    const colorPicker = document.getElementById('colorPicker');
+    const cell = document.getElementsByClassName('cell');
+
+    for(let item = 0; item < cell.length; item++) {
+        cell[item].addEventListener("click", function (event) {
+            let cellClicked = event.target;
+            cellClicked.style.backgroundColor = colorPicker.value;
+        });
+    }
 }
 
 // Apply submit to size picker
 document.getElementById('sizePicker').onsubmit = function () {
     userSubmit();
 };
-
-// Build a default 10x10 grid.
